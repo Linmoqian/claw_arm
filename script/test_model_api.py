d9ac33b06f0a8909a9a5fc98a49219c5779b7bce
Linmoqian <3047076929@qq.com>
2026-03-09 17:29:28 +0800

    添加了点细节

--- script/test_model_api.py
+++ script/test_model_api.py
@@ -4,7 +4,7 @@ from openai import OpenAI
 try:
     client = OpenAI(
         # 若没有配置环境变量，请用阿里云百炼API Key将下行替换为: api_key="sk-xxx",
-        api_key="",
+        api_key="替换为你的阿里云百炼API Key",
         base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
     )
 
